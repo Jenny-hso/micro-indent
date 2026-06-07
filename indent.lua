94eea52f0016d6cc52f6d566e487825f08d0d74b
Jenny-hso <260111573+Jenny-hso@users.noreply.github.com>
2026-06-07 17:40:10 +0300

    Move indentation decision logic into a separate function.

--- indent.lua
+++ indent.lua
@@ -4,11 +4,11 @@ local micro  = import("micro")
 local config = import("micro/config")
 local regexp = import("regexp")
 
-function preInsertNewline(bp)
+function shouldIndent(bp)
    local regex = bp.Buf.Settings["indent.regex"]
 
    if regex == "" then
-      return true
+      return false
    end
 
    local line = bp.Buf:Line(bp.Cursor.Y):sub(1, bp.Cursor.X)
@@ -17,10 +17,18 @@ function preInsertNewline(bp)
 
    if err ~= nil then
       micro.InfoBar():Error(err)
-      return true
+      return false
    end
 
    if not matched then
+      return false
+   end
+
+   return true
+end
+
+function preInsertNewline(bp)
+   if not shouldIndent(bp) then
       return true
    end
 
